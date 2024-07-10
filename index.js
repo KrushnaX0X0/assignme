880e474c688bb0169e1a4be6fc6e1d89ff131fdb
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2024-07-10 17:29:26 +0530

    add js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-let marks [80,70,60,90,95,99]
-console.log(marks)
+let marks = [80, 70, 60, 90, 95 ,99]
+
+let largest = marks[0]
+
+for (let i=1; i <marks.length;  i++){
+
+if(marks[i] > largest){
+    largest = marks[i]
+}
+
+
+}
+console.log(largest)
