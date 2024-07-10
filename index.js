5c0ae86b81da1bdcf23bf9b224e10989e013a035
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2024-07-10 17:48:40 +0530

    add js code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let marks = [80, 70, 60, 90, 95 ,99]
 
 let largest = marks[0]
 
+
 for (let i=1; i <marks.length;  i++){
 
 if(marks[i] > largest){
@@ -10,4 +11,21 @@ if(marks[i] > largest){
 
 
 }
-console.log(largest)
+console.log(`The array largest number ${largest}`)
+
+
+
+let sum = 0;
+for (let i=0; i < marks.length; i++){
+    if(marks[i] %2==1){
+        sum += marks[i]
+    }
+}
+console.log(`odd addtion ${sum}`)
+
+
+let student = ["krushna","pavan","sakshi","radha","guru"]
+
+for(let i=0; i<student.length; i++){
+    console.log(`hello ${student[i]}`)
+}
